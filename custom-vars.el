4813f4bae02f4976609669835c8bfb766d74c693
Derek Ealy <derek.ealy@spirent.com>
2025-02-13 08:44:00 -0800

    extracted out early-init.el

--- custom-vars.el
+++ custom-vars.el
@@ -4,9 +4,6 @@
 (setq inhibit-startup-message t
       visible-bell t)
 
-;; prep for straight.el later
-(setq package-enable-at-startup nil)
-
 ;; Turn off some unneeded UI elements
 (menu-bar-mode -1)  ; Leave this one on if you're a beginner!
 (tool-bar-mode -1)
