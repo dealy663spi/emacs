b9dc268164aa1eafc1c43917c25ce38b82b74b8c
Derek Ealy <derek.ealy@spirent.com>
2025-04-25 08:49:57 -0700

    fix for bad-signature problem

--- custom-vars.el
+++ custom-vars.el
@@ -37,9 +37,11 @@
 
 (package-initialize)
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
 
-(require 'use-package)
+(eval-when-compile
+  (require 'use-package))
 (setq use-package-always-ensure t)
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
